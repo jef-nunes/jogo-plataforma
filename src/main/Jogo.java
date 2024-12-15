8235a90834a8848e7d368bf87a43c39418d23c0b
Jeferson <159483664+jef-nunes@users.noreply.github.com>
2024-12-15 12:10:22 -0300

    Add files via upload

--- src/main/Jogo.java
+++ src/main/Jogo.java
@@ -1,15 +1,48 @@
 package main;
-import java.util.Map;
-import java.util.HashMap;
 
-public class Jogo
+public class Jogo implements Runnable
 {
 	private Janela janela;
+	private Painel painel;
+	private Thread threadJogo;
+	private final int FPS_SET = 120;
 	public Jogo()
 	{
-		Painel painel = new Painel();
-		Janela janela = new Janela(painel);
-		painel.requestFocus();
+		this.painel = new Painel();
+		this.janela = new Janela(painel);
+		this.painel.requestFocus();
+		this.startGameLoop();
+	}
+	@Override
+	public void run()
+	{
+		double tempoPorFrame = 1000000000.0 / FPS_SET;
+		long ultimoFrame = System.nanoTime();
+		long agora = System.nanoTime();
+		int frames = 0;
+		long ultimoCheck = System.currentTimeMillis();
+		while(true)
+		{
+			agora = System.nanoTime();
+			if(agora - ultimoFrame >= tempoPorFrame)
+			{
+				this.painel.repaint();
+				ultimoFrame = agora;
+				frames++;
+			}
+			
+			if(System.currentTimeMillis() - ultimoCheck >= 1000)
+			{
+				ultimoCheck = System.currentTimeMillis();
+				System.out.println("FPS: "+frames);
+				frames=0;
+			}
+		}
+	}
+	private void startGameLoop()
+	{
+		this.threadJogo = new Thread(this);
+		this.threadJogo.start();
 	}
 	public void printStatus(){
 		if(janela != null){
--- src/main/Painel.java
+++ src/main/Painel.java
@@ -1,16 +1,25 @@
 package main;
+import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
 import javax.swing.JPanel;
 import inputs.Mouse;
 import inputs.Teclado;
 
 public class Painel extends JPanel
 {
+	private Color color = new Color(150,20,90);
 	private Mouse mouse;
-	private int deltaX = 0;
-	private int deltaY = 0;
+	private float deltaX = 100f;
+	private float deltaY = 100f;
+	private float dirX = 0.003f;
+	private float dirY = 0.003f;
+	private int frames = 0;
+	private long lastCheck = 0;
+	private Random random;
 	public Painel()
 	{
+		random = new Random();
 		mouse = new Mouse(this);
 		addMouseListener(mouse);
 		addMouseMotionListener(mouse);
@@ -19,23 +28,41 @@ public class Painel extends JPanel
 	public void updateDeltaX(int value)
 	{
 		this.deltaX += value;
-		repaint();
 	}
 	public void updateDeltaY(int value)
 	{
 		this.deltaY += value;
-		repaint();
 	}
 	public void setRectPos(int x, int y)
 	{
 		this.deltaX = x;
 		this.deltaY = y;
-		repaint();
 	}
 	public void paintComponent(Graphics g)
 	{
+		updateRectangle();
+		g.setColor(this.color);
 		super.paintComponent(g);
-		g.fillRect(this.deltaX, this.deltaY, 50, 50);
-		
+		g.fillRect((int)this.deltaX, (int)this.deltaY, 50, 50);
+	}
+	private void updateRectangle()
+	{
+		this.deltaX += dirX;
+		if(deltaX >= 325 || deltaX <= 0){
+			dirX *= -1;
+			this.color = getRectColor();
+		}
+		this.deltaY += dirY;
+		if(deltaY >= 325 || deltaY <= 0){
+			dirY *= -1;
+			this.color = getRectColor();
+		}
+	}
+	private Color getRectColor()
+	{
+		int r = random.nextInt(255);
+		int g = random.nextInt(255);
+		int b = random.nextInt(255);
+		return new Color(r,g,b);
 	}
 }
